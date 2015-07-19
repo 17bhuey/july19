26a2def6e04060fd12518d7fa1e30a78c948ee4a
Ryan A. Transfiguracion <rtransfig@gmail.com>
2015-07-19 14:47:29 -0400

    starting on wordLadder

---
+++ wordLadder.py
@@ -0,0 +1,13 @@
+def wordLadder(word1,word2):
+	print "hello there"
+
+	if word1 == word2:
+		print "the words cannot be the same"
+	if len(word1) != len(word2):
+		print "the words must be the same length"
+
+
+
+wordLadder("code","data")
+
+wordLadder("code","datas")
